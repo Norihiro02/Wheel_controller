0bfb27410b06a793938a07ed9f49b2b04dd6a340
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-08-02 00:29:33 +0900

    骨組みを作成
    メカナムはタイヤ4つで固定なので結構楽

---
+++ src/Mecanum_wheel_vel.cpp
@@ -0,0 +1,12 @@
+class Mecanum_vel{
+    double a,b;
+    Mecanum_vel(){
+
+    }
+    void getWheelSpeed(double x,double y,double rot){
+        //平行移動だけ計算
+        //回転運動分を計算
+        //平行運動と回転運動を加算する
+    }
+
+}
