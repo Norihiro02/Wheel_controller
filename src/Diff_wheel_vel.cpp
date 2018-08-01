6277ad8a65ccfe2af04d4adaa7ce7d7bfe04405a
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-08-01 23:43:56 +0900

    コメントの追加と数式の修正

--- src/Diff_wheel_vel.cpp
+++ src/Diff_wheel_vel.cpp
@@ -2,13 +2,16 @@
 class deff_wheel{
     double vMotorR,vMotorL;
     double R;
-    deff_wheel(double d){
+    //車輪の間隔を入力
+    deff_wheel(double l){
         vMotorR=0;
-        MotorL=0;
-        R=d/2;
+        vMotorL=0;
+        R=l/2;
     }
+    // v:ロボットの移動速度
+    // rads:ロボットの回転速度
     void MotorSpead(double v,double rads){
-        vMotorR=vMotorL=v;
+        vMotorR=vMotorL=v/2;
         vMotorR += rads*R;
         vMotorL -= rads*R;
     }
